3f8ec58ce35432e63d98a47c058e0ce9c3a290a0
juanrm29 <juanmaulana29@gmail.com>
2024-02-07 01:29:05 +0800

    gas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <div className="h-fit w-1/2 p-4 flex flex-col items-center justify-center">
         <h1 className="text-4xl font-medium">Met Datang di Julink!</h1>
         <h3 className="mt-2 mb-2 text-lg text-center ">
-        Simplify your online presence with itZmyLink! Create a personalized page to showcase all your social media profiles in one place.
+        Simplify your online presence with Julink! Create a personalized page to showcase all your social media profiles in one place.
         </h3>
         <Button variant={"custom"}>
           <Link
